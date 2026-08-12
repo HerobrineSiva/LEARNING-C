e2ef93d9e200e084859975778f84407cff26469d
HerobrineSiva <sivasakthirajanssiva@gmail.com>
2026-08-12 16:19:00 +0530

    simple first task

---
+++ calculator1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (void)
+{
+    int maths=82,physics=78,chemistry=85,total;
+    float average;
+    total=maths+physics+chemistry;
+    average=total/3;
+    printf("The Total is %d\nThe average is %f",total,average);
+    return 0;
+}
