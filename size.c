d71e7850a41ce0a11c3db95ff4e08743673bd9b1
HerobrineSiva <sivasakthirajanssiva@gmail.com>
2026-08-12 15:45:17 +0530

    clearing all

--- size.c
+++ size.c
@@ -4,6 +4,6 @@
         printf("Size of char: %zu byte(s)\n", sizeof(char));
         printf("Size of int: %zu byte(s)\n", sizeof(int));
         printf("Size of float: %zu byte(s)\n", sizeof(float));
-        printf("Size of  double: %zu byte(s)\n", sizeof(double));
+        printf("Size of double: %zu byte(s)\n", sizeof(double));
         return 0;
     }
